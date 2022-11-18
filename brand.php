c7ff0e75f5ba26cccf05ccbde1131f4ef4b881cc
rajaabdulsubhan <89687398+rajaabdulsubhan@users.noreply.github.com>
2022-11-18 12:49:41 -0700

    done

--- brand.php
+++
@@ -1,3 +0,0 @@
-<?php
-	$domainName = "ConvertPNGtoJPGOnline.com";
-?>
--- index.php
+++ index.php
@@ -1,71 +1,38 @@
-<?php include("./brand.php"); ?>
-<?php include("./helper.php"); ?>
-
-<!DOCTYPE html>
+<?php
+	if (isset($_GET['function'])) {
+		require_once 'application/controllers/EpubController.php';
+		require_once 'application/controllers/MobiController.php';
+		require_once 'application/controllers/PdfController.php';
+		require_once('application/Bootstrap.php');
+		
+		$function = ucfirst($_GET['function']);
+		if (isset($_GET['str'])) $options = array("html" => "<h1>Hello, World</h1><p><strong>Here is some text, wrapped in 'p' and 'strong' HTML tags</strong></p><p><em>Here is some text, wrapped in 'p' and 'em' HTML tags</em></p>");
+		$controllerName = "{$function}Controller";
+		$modelName = "{$function}Model";
+		$actionName = "create{$function}Action";
+		
+		//use variable variables based on $_GET variable to instantiate class and call method
+		$bootstrap = Bootstrap::singleton();
+		$tools = $bootstrap->getTools(); 
+		$transform = new TransformModel();
+		
+		$model = new $modelName($tools);
+		$controller = new $controllerName($model, $tools, $transform);
+		$controller->$actionName($options);
+		exit();
+	}
+?>
 <html>
-<head>
-	<?php include("./head.php"); ?>
-	<title></title>
-	<meta name="description" content="">
-	<meta name="keywords" content="">
-</head>
+<head></head>
+<link rel="stylesheet" href="bootstrap.css" />
 <body>
-<?php include("./header.php"); ?>
-
-<div class="container">
-	
-	<div class="row">
-		<p>This free online tool converts your PNG images to JPEG format, applying proper compression methods. Unlike other services, this tool does not ask for your email address, offers mass conversion and allows files up to 50 MB.</p>
+	<div class="container">
+	<?php
+		//a VERY simple page router/despatcher to include the 'default' view if the index.php file is requested
+		if (strtolower($_SERVER['PHP_SELF']) == "/digitalformatconvert/index.php") {
+			include 'application/views/default.php';
+		}
+	?>
 	</div>
-	
-	<div class='row' style='border: 1px solid #CCC; margin: 10px; padding: 10px; box-shadow: 2px 2px 0px 0px #CCC; border-radius: 10px; overflow: hidden;'>	
-		<div style='text-align: center; margin: 10px;'>
-			<?php
-				if(isset($_POST['submit'])) {
-					uploadAndConvert();
-				}
-			?>
-		</div>
-
-		<div class="row" style='text-align: center; border: 1px dashed #CCC; padding: 20px; max-width: 1200px; margin: 20px auto; border-radius: 10px;'>
-			<form action="" method="post" enctype="multipart/form-data">
-				<input type="file" class="form-control-file" name="fileToUpload" id="fileToUpload" required />
-				<input class="btn btn-primary" type="submit" value="Upload Image" name="submit" style='margin: 10px;' />
-			</form>
-		</div>
-	</div>
-	
-	<hr />
-	
-	<div class="row">
-		<img src='./img/main_page.png' style="max-width: 750px; margin-bottom: 20px;" class="rounded mx-auto d-block" alt="" />
-		<h2 class='display-6'>How to convert PNG to JPG</h2>
-		<p>
-			<ol>
-				<li>Upload any PNG format image you want to convert into JPG.</li>
-				<li>Click on Upload Image and the Uploading will start.</li>
-				<li>The PNG format image will automatically convert to JPG format and a JPG format image download link will be provided.</li>
-				<li>Click on "Download Image in JPG" to download your converted image for free.</li>
-			</ol>
-		</p>
-		
-		
-		<h2 class='display-6'>How can I convert PNG to JPG for free?</h2>
-		<p>
-			<ol>
-				<li>Upload any PNG format image you want to convert into JPG.</li>
-				<li>Click on Upload Image and the Uploading will start.</li>
-				<li>The PNG format image will automatically convert to JPG format and a JPG format image download link will be provided.</li>
-				<li>Click on "Download Image in JPG" to download your converted image for free.</li>
-			</ol>
-		</p>
-	</div>
-</div>
-	
-<?php include("./footer.php"); ?>
-
-
-
-
 </body>
 </html>
